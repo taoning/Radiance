6d45d8e830c10c15a173d0ae15f4e5bf24e84b3e
Taoning Wang <taoning@gmail.com>
2024-03-23 09:57:41 -0700

    update

--- src/gen/sun.h
+++ src/gen/sun.h
@@ -9,37 +9,19 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-typedef struct {
-  int year;
-  int month;
-  int day;
-  float hour;
-} Datetime;
-
-typedef struct {
-  double latitude;
-  double longitude;
-  double meridian;
-} Location;
-/* sun calculation constants */
-extern double s_latitude;
-extern double s_longitude;
-extern double s_meridian;
+					/* sun calculation constants */
+extern double  s_latitude;
+extern double  s_longitude;
+extern double  s_meridian;
 
 extern int jdate(int month, int day);
-extern double stadj(const int jd, const double s_meridian,
-                    const double s_longitude);
-extern double sdec(const int jd);
-extern double salt(const double sd, const double st, const double s_latitude);
-extern double sazi(const double sd, const double st, const double s_latitude);
+extern double stadj(int  jd);
+extern double sdec(int  jd);
+extern double salt(double sd, double st);
+extern double sazi(double sd, double st);
 
-extern double mjdate(const int year, const int month, const int day,
-                     double hour, const double s_meridian);
-extern double msdec(const double mjd, double *stp, const double s_longitude);
-
-extern int compute_sundir(const Datetime *dt, const Location *loc,
-                          const int tsolar, double sundir[3]);
+extern double mjdate(int year, int month, int day, double hour);
+extern double msdec(double mjd, double *stp);
 
 #ifdef __cplusplus
 }
